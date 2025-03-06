1c9c68e9043d301ea249bab48a1f95e7b0106b32
ayabaara <ayabaara4@gmail.com>
2025-03-06 15:03:48 +0200

    generate constructor for Passengers

--- AirportBooking/models/Passenger.cs
+++ AirportBooking/models/Passenger.cs
@@ -12,5 +12,13 @@ namespace AirportBooking.models
         public string Name { get; set; }
         public string Email { get; set; }
         public int PhoneNumber { get; set; }
+
+        public Passenger(int id, string name, string email, int phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
     }
 }
