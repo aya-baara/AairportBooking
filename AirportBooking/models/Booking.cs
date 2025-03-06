fea2c9c507d4983d64ece57c62e5e3c71f01a910
ayabaara <ayabaara4@gmail.com>
2025-03-06 13:18:46 +0200

    add Booking model

---
+++ AirportBooking/models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.models
+{
+    class Booking
+    {
+        public int BookingId { get; set; }  
+        public int PassengerId { get; set; } 
+        public int FlightId { get; set; }  
+        public string ClassType { get; set; }  
+        public decimal Price { get; set; }  
+        public DateTime BookingDate { get; set; }
+    }
+}
