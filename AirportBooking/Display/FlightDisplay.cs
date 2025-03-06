6c885de410f9095c6143f143ae9095f2aedf2a55
ayabaara <ayabaara4@gmail.com>
2025-03-06 13:13:22 +0200

    add display for passenger and flight models

---
+++ AirportBooking/Display/FlightDisplay.cs
@@ -0,0 +1,32 @@
+﻿using AirportBooking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Display
+{
+    class FlightDisplay
+    {
+        public static string GetFlightDetails(Flight flight)
+        {
+            return $"""
+        Flight Details =>
+        Flight ID: {flight.FlightId}
+        Airline: {flight.Airline}
+        From: {flight.DepartureCountry} ({flight.DepartureAirport})
+        To: {flight.DestinationCountry} ({flight.ArrivalAirport})
+        Departure: {flight.DepartureDate}
+        Arrival: {flight.ArrivalDate}
+        Available Seats: {flight.AvailableSeats}
+        
+        Ticket Prices:
+        - Economy: ${flight.EconomyPrice}
+        - Business: ${flight.BusinessPrice}
+        - First Class: ${flight.FirstClassPrice}
+        """;
+        }
+
+    }
+}
---
+++ AirportBooking/Display/PassengerDisplay.cs
@@ -0,0 +1,23 @@
+﻿using AirportBooking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Display
+{
+    class PassengerDisplay
+    {
+        public static string GetPassengerDetails(Passenger passenger)
+        {
+            return $"""
+        Passenger Details => 
+        ID: {passenger.Id}, 
+        Name: {passenger.Name}, 
+        Email: {passenger.Email}, 
+        Phone: {passenger.PhoneNumber}
+        """;
+        }
+    }
+}
