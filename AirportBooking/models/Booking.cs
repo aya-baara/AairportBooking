1044be26c6fdb5caf7c887c7648f7bab53c1a04d
ayabaara <ayabaara4@gmail.com>
2025-03-06 15:35:14 +0200

    add enum for flight class type & edit Booking to use it

--- AirportBooking/models/Booking.cs
+++ AirportBooking/models/Booking.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirportBooking.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,11 @@ namespace AirportBooking.models
         public int BookingId { get; set; }  
         public int PassengerId { get; set; } 
         public int FlightId { get; set; }  
-        public string ClassType { get; set; }  
+        public ClassType ClassType { get; set; }  
         public decimal Price { get; set; }  
         public DateTime BookingDate { get; set; }
 
-        public Booking(int passengerId, int flightId, string classType, decimal price, DateTime bookingDate)
+        public Booking(int passengerId, int flightId, ClassType classType, decimal price, DateTime bookingDate)
         {
             BookingId = _nextBookingId++;
             PassengerId = passengerId;
