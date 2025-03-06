00caa43d8f21a8b50e131630f14188e2bb2b58a4
ayabaara <ayabaara4@gmail.com>
2025-03-06 12:49:38 +0200

    add Flight model

---
+++ AirportBooking/models/Flight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.models
+{
+    class Flight
+    {
+        public int FlightId { get; set; } 
+        public string Airline { get; set; }  
+        public string DepartureCountry { get; set; }
+        public string DestinationCountry { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public int AvailableSeats { get; set; }  
+        public decimal EconomyPrice { get; set; }
+        public decimal BusinessPrice { get; set; }
+        public decimal FirstClassPrice { get; set; }
+    }
+}
