a5bf29ce27b8a1342837651af3a447dbf6b0eba9
ayabaara <ayabaara4@gmail.com>
2025-03-10 10:18:22 +0200

    Add passenger's  Booking service Book,cancel,view personal booking

---
+++ AirportBooking/Comparer/BookingComparer.cs
@@ -0,0 +1,32 @@
+﻿using AirportBooking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Comparer
+{
+    public class BookingComparer : IEqualityComparer<Booking>
+    {
+        public bool Equals(Booking x, Booking y)
+        {
+            if (x == null || y == null)
+                return false;
+
+            return (x.FlightId== y.FlightId) && (x.PassengerId == y.PassengerId);
+        }
+
+        int IEqualityComparer<Booking>.GetHashCode(Booking obj)
+        {
+            if (obj == null)
+                return 0;
+
+            int bookingIdHashCode = obj.FlightId.GetHashCode();
+            int passengerIdHashCode = obj.PassengerId.GetHashCode();
+
+            // Combine them using XOR (^) or addition
+            return bookingIdHashCode ^ passengerIdHashCode;
+        }
+    }
+}
---
+++ AirportBooking/Services/PassengerServices/BookingService.cs
@@ -0,0 +1,41 @@
+﻿using AirportBooking.Comparer;
+using AirportBooking.Data;
+using AirportBooking.Enums;
+using AirportBooking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Services.PassengerServices
+{
+    class BookingService
+    {
+        public void BookFlight(Passenger passenger,Flight flight, ClassType classType) {
+            DataStore.Bookings.Add(new Booking(
+                    passenger.Id, flight.FlightId, classType, flight.ClassPrices.GetValueOrDefault(classType,0), DateTime.Now
+                ));
+
+        
+        }
+
+        public bool CancelBooking(Passenger passenger, Flight flight)
+        {
+            var booking= DataStore.Bookings.FirstOrDefault(booking => booking.PassengerId == passenger.Id);
+            if (booking == null)
+                return false; // Booking not found
+
+            return DataStore.Bookings.Remove(booking);
+                  
+
+        }
+
+        public List<Booking>ViewPersonalBooking(Passenger passenger)
+        {
+            return DataStore.Bookings.Where(booking => booking.PassengerId == passenger.Id).ToList();
+        }
+
+       
+    }   
+}
