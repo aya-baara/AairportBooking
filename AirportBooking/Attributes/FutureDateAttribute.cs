3b474ceb01e5c5850ff428af044c44e088c541c6
ayabaara <ayabaara4@gmail.com>
2025-03-10 15:28:21 +0200

    add custom attribute FutureDateAttribute

---
+++ AirportBooking/Attributes/FutureDateAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Attributes
+{
+    class FutureDateAttribute : ValidationAttribute
+    {
+        public FutureDateAttribute()
+        {
+            ErrorMessage = "Departure date must be in the future.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateTime)
+            {
+                if (dateTime < DateTime.Now)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
