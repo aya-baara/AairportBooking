28b24f6b33f7fe81068552cb286b76ad80eeac6f
ayabaara <ayabaara4@gmail.com>
2025-03-10 14:22:13 +0200

    crete BookingFilterParameter record & booking search method

---
+++ AirportBooking/Records/BookingFilterParamters.cs
@@ -0,0 +1,26 @@
+﻿using AirportBooking.Enums;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AirportBooking.Records
+{
+    public record BookingFilterParamters(
+        int? PassengerId = null ,
+        int? FlightId = null ,
+        ClassType? ClassType = null ,
+        decimal? Price = null ,
+        DateTime? BookingDate = null ,
+        string? DepartureCountry = null,
+        string? DestinationCountry = null,
+        DateTime? DepartureDate = null,
+        string? DepartureAirport = null,
+        string? ArrivalAirport = null
+
+        );
+    
+}
---
+++ AirportBooking/Services/ManagerServices/BookingSearchService.cs
@@ -0,0 +1,82 @@
+﻿using AirportBooking.Data;
+using AirportBooking.models;
+using AirportBooking.Records;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Services.ManagerServices
+{
+    class BookingSearchService
+    {
+        public List<Booking> SearchBookings(BookingFilterParamters bookingFilter)
+        {
+            
+
+            
+            var query = from booking in DataStore.Bookings
+                        join flight in DataStore.flights on booking.FlightId equals flight.FlightId
+                        select new { booking, flight };
+
+            
+            if (bookingFilter.PassengerId.HasValue)
+            {
+                query = query.Where(x => x.booking.PassengerId == bookingFilter.PassengerId.Value);
+            }
+
+            if (bookingFilter.FlightId.HasValue)
+            {
+                query = query.Where(x => x.booking.FlightId == bookingFilter.FlightId.Value);
+            }
+
+            if (bookingFilter.ClassType.HasValue)
+            {
+                query = query.Where(x => x.booking.ClassType == bookingFilter.ClassType.Value);
+            }
+
+            if (bookingFilter.Price.HasValue)
+            {
+                query = query.Where(x => x.booking.Price == bookingFilter.Price.Value);
+            }
+
+            if (bookingFilter.BookingDate.HasValue)
+            {
+                query = query.Where(x => x.booking.BookingDate.Date == bookingFilter.BookingDate.Value.Date);
+            }
+
+            if (!string.IsNullOrEmpty(bookingFilter.DepartureCountry))
+            {
+                query = query.Where(x => x.flight.DepartureCountry.Contains(bookingFilter.DepartureCountry));
+            }
+
+            if (!string.IsNullOrEmpty(bookingFilter.DestinationCountry))
+            {
+                query = query.Where(x => x.flight.DestinationCountry.Contains(bookingFilter.DestinationCountry));
+            }
+
+            if (bookingFilter.DepartureDate.HasValue)
+            {
+                query = query.Where(x => x.flight.DepartureDate.Date == bookingFilter.DepartureDate.Value.Date);
+            }
+
+            if (!string.IsNullOrEmpty(bookingFilter.DepartureAirport))
+            {
+                query = query.Where(x => x.flight.DepartureAirport.Contains(bookingFilter.DepartureAirport));
+            }
+
+            if (!string.IsNullOrEmpty(bookingFilter.ArrivalAirport))
+            {
+                query = query.Where(x => x.flight.ArrivalAirport.Contains(bookingFilter.ArrivalAirport));
+            }
+
+            var filteredBookings = query.Select(x => x.booking).ToList();
+
+            return filteredBookings;
+        }
+    }
+
+
+}
