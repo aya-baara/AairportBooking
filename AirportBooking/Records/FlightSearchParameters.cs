5ca6b770d40d81c8910e578fec8bbe2e71217993
ayabaara <ayabaara4@gmail.com>
2025-03-10 12:10:11 +0200

    Add FlightSearchParameters record

---
+++ AirportBooking/Records/FlightSearchParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Records
+{
+    public record FlightSearchParameters
+    (
+        string? Airline = null ,
+        string? DepartureCountry = null,
+        string? DestinationCountry = null,
+        string? DepartureAirport = null,
+        string? ArrivalAirport = null,
+        DateTime? DepartureDate = null,
+        DateTime? ArrivalDate = null,
+        int ? AvailableSeats = null,
+        int ? Price = null
+    );
+}
