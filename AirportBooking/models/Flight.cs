b9439a6670cc32cd0babab4691f08d9fb55ae2f1
ayabaara <ayabaara4@gmail.com>
2025-03-10 15:29:50 +0200

    add attribute to validate flight properties

--- AirportBooking/models/Flight.cs
+++ AirportBooking/models/Flight.cs
@@ -1,6 +1,8 @@
-﻿using AirportBooking.Enums;
+﻿using AirportBooking.Attributes;
+using AirportBooking.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +12,36 @@ namespace AirportBooking.models
     public class Flight
     {
         private static int _nextId = 1;
-        public int FlightId { get; set; } 
-        public string Airline { get; set; }  
+        [Required(ErrorMessage = "Departure Flight ID is required.")]
+        public int FlightId { get; set; }
+
+        [Required(ErrorMessage = "Airline is required.")]
+        public string Airline { get; set; }
+
+        [Required(ErrorMessage = "Departure country is required.")]
         public string DepartureCountry { get; set; }
+
+        [Required(ErrorMessage = "Destination country is required.")]
         public string DestinationCountry { get; set; }
+
+        [Required(ErrorMessage = "Departure airport is required.")]
         public string DepartureAirport { get; set; }
+
+        [Required(ErrorMessage = "Arrival airport is required.")]
         public string ArrivalAirport { get; set; }
+
+        [Required(ErrorMessage = "Departure date is required.")]
+        [DataType(DataType.DateTime)]
+        [FutureDateAttribute]
         public DateTime DepartureDate { get; set; }
+
+        [Required(ErrorMessage = "Arrival date is required.")]
+        [DataType(DataType.DateTime)]
         public DateTime ArrivalDate { get; set; }
+
+        [Range(1, 300, ErrorMessage = "Available seats must be between 1 and 300.")]
         public int AvailableSeats { get; set; }
+
         public Dictionary<ClassType, decimal> ClassPrices { get; set; }
 
         public Flight()
