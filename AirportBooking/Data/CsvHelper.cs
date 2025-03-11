e3be1287a14b979b4a0fd9c9e40734185030a4d5
ayabaara <ayabaara4@gmail.com>
2025-03-11 10:53:27 +0200

    use csvHelper to build readFrom & writeTo methods

---
+++ AirportBooking/Data/CsvHelper.cs
@@ -0,0 +1,50 @@
+﻿using AirportBooking.models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+using System.IO;
+namespace AirportBooking.Data
+{
+    class CsvFileHelper
+    {
+        public static List<T> ReadFromFile<T>(string filePath)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HeaderValidated = null,
+                MissingFieldFound = null
+            };
+
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, config))
+            {
+                // Apply FlightMap only if T is Flight
+                if (typeof(T) == typeof(Flight))
+                {
+                    csv.Context.RegisterClassMap<FlightMap>();
+                }
+
+                return csv.GetRecords<T>().ToList();
+            }
+        }
+
+        public static void WriteToFile<T>(List<T> records, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                // Apply FlightMap only if T is Flight
+                if (typeof(T) == typeof(Flight))
+                {
+                    csv.Context.RegisterClassMap<FlightMap>();
+                }
+
+                csv.WriteRecords(records);
+            }
+        }
+
+
+    }
+
+}
