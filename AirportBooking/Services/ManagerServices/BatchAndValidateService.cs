5b258eac0b761641cfb70f85a63e3aa83e5e9543
ayabaara <ayabaara4@gmail.com>
2025-03-11 12:35:55 +0200

    add method to read flights and validate them

---
+++ AirportBooking/Services/ManagerServices/BatchAndValidateService.cs
@@ -0,0 +1,33 @@
+﻿using AirportBooking.Data;
+using AirportBooking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking.Services.ManagerServices
+{
+    class BatchAndValidateService
+    {
+        public static Dictionary<string, List<string>> GetValidationFlightRules()
+        {
+            return ValidationHelperService.GetValidationRules(typeof(Flight));
+        }
+
+        public static List<string> ReadAndValidate(string filePath)
+        {
+            List<string> allErrors = new List<string>(); 
+            List<Flight> flights = CsvFileHelper.ReadFromFile<Flight>(filePath);
+
+            foreach (var f in flights)
+            {
+                List<string> errors = ValidationHelperService.ValidateModel(f);
+                allErrors.AddRange(errors);
+            }
+
+            return allErrors; 
+        }
+
+    }
+}
