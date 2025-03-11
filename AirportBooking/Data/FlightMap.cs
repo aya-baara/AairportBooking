185b0d8cd0a37438e32207a34681a11c5532b8d3
ayabaara <ayabaara4@gmail.com>
2025-03-11 10:54:18 +0200

    add mapper for flight to handle calssPrices dictionary

---
+++ AirportBooking/Data/FlightMap.cs
@@ -0,0 +1,27 @@
+﻿using AirportBooking.models;
+using CsvHelper.Configuration;
+using AirportBooking.Enums;
+
+
+namespace AirportBooking.Data
+{
+    public class FlightMap : ClassMap<Flight>
+    {
+        public FlightMap()
+        {
+            Map(f => f.FlightId);
+            Map(f => f.Airline);
+            Map(f => f.DepartureCountry);
+            Map(f => f.DestinationCountry);
+            Map(f => f.DepartureAirport);
+            Map(f => f.ArrivalAirport);
+            Map(f => f.DepartureDate);
+            Map(f => f.ArrivalDate);
+            Map(f => f.AvailableSeats);
+            Map(f => f.BusinessPrice).Name("BusinessPrice");
+            Map(f => f.EconomyPrice).Name("EconomyPrice");
+            Map(f => f.FirstClassPrice).Name("FirstClassPrice");
+        }
+    }
+
+}
