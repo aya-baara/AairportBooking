b3aa15ecfe16c50d5696bcd6625d474c0fa25a2e
ayabaara <ayabaara4@gmail.com>
2025-03-13 13:07:19 +0200

    Refactor SearchBookings method to use ternary operator for filters

--- AirportBooking/Services/ManagerServices/BookingSearchService.cs
+++ AirportBooking/Services/ManagerServices/BookingSearchService.cs
@@ -9,63 +9,22 @@ namespace AirportBooking.Services.ManagerServices
     {
         public List<Booking> SearchBookings(BookingFilterParamters bookingFilter)
         {
-            
-
-            
             var query = from booking in DataStore.Bookings
                         join flight in DataStore.Flights on booking.FlightId equals flight.FlightId
                         select new { booking, flight };
 
-            
-            if (bookingFilter.PassengerId.HasValue)
-            {
-                query = query.Where(x => x.booking.PassengerId == bookingFilter.PassengerId.Value);
-            }
-
-            if (bookingFilter.FlightId.HasValue)
-            {
-                query = query.Where(x => x.booking.FlightId == bookingFilter.FlightId.Value);
-            }
-
-            if (bookingFilter.ClassType.HasValue)
-            {
-                query = query.Where(x => x.booking.ClassType == bookingFilter.ClassType.Value);
-            }
-
-            if (bookingFilter.Price.HasValue)
-            {
-                query = query.Where(x => x.booking.Price == bookingFilter.Price.Value);
-            }
-
-            if (bookingFilter.BookingDate.HasValue)
-            {
-                query = query.Where(x => x.booking.BookingDate.Date == bookingFilter.BookingDate.Value.Date);
-            }
-
-            if (!string.IsNullOrEmpty(bookingFilter.DepartureCountry))
-            {
-                query = query.Where(x => x.flight.DepartureCountry.Contains(bookingFilter.DepartureCountry));
-            }
-
-            if (!string.IsNullOrEmpty(bookingFilter.DestinationCountry))
-            {
-                query = query.Where(x => x.flight.DestinationCountry.Contains(bookingFilter.DestinationCountry));
-            }
-
-            if (bookingFilter.DepartureDate.HasValue)
-            {
-                query = query.Where(x => x.flight.DepartureDate.Date == bookingFilter.DepartureDate.Value.Date);
-            }
-
-            if (!string.IsNullOrEmpty(bookingFilter.DepartureAirport))
-            {
-                query = query.Where(x => x.flight.DepartureAirport.Contains(bookingFilter.DepartureAirport));
-            }
-
-            if (!string.IsNullOrEmpty(bookingFilter.ArrivalAirport))
-            {
-                query = query.Where(x => x.flight.ArrivalAirport.Contains(bookingFilter.ArrivalAirport));
-            }
+            query = query.Where(x =>
+                (bookingFilter.PassengerId.HasValue ? x.booking.PassengerId == bookingFilter.PassengerId.Value : true) &&
+                (bookingFilter.FlightId.HasValue ? x.booking.FlightId == bookingFilter.FlightId.Value : true) &&
+                (bookingFilter.ClassType.HasValue ? x.booking.ClassType == bookingFilter.ClassType.Value : true) &&
+                (bookingFilter.Price.HasValue ? x.booking.Price == bookingFilter.Price.Value : true) &&
+                (bookingFilter.BookingDate.HasValue ? x.booking.BookingDate.Date == bookingFilter.BookingDate.Value.Date : true) &&
+                (string.IsNullOrEmpty(bookingFilter.DepartureCountry) ? true : x.flight.DepartureCountry.Contains(bookingFilter.DepartureCountry)) &&
+                (string.IsNullOrEmpty(bookingFilter.DestinationCountry) ? true : x.flight.DestinationCountry.Contains(bookingFilter.DestinationCountry)) &&
+                (bookingFilter.DepartureDate.HasValue ? x.flight.DepartureDate.Date == bookingFilter.DepartureDate.Value.Date : true) &&
+                (string.IsNullOrEmpty(bookingFilter.DepartureAirport) ? true : x.flight.DepartureAirport.Contains(bookingFilter.DepartureAirport)) &&
+                (string.IsNullOrEmpty(bookingFilter.ArrivalAirport) ? true : x.flight.ArrivalAirport.Contains(bookingFilter.ArrivalAirport))
+            );
 
             var filteredBookings = query.Select(x => x.booking).ToList();
 
