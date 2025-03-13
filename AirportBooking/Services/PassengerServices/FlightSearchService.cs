1ac3e439f41d5278de215f3944acee8b54569b90
ayabaara <ayabaara4@gmail.com>
2025-03-13 12:39:11 +0200

    Refactor SearchFlights method to use ternary operator for filtering

--- AirportBooking/Services/PassengerServices/FlightSearchService.cs
+++ AirportBooking/Services/PassengerServices/FlightSearchService.cs
@@ -9,47 +9,22 @@ namespace AirportBooking.Services.PassengerServices
     {
         public List<Flight> SearchFlights(FlightSearchParameters flightSearchParameters)
         {
-            IEnumerable<Flight> query = DataStore.Flights;
-
-            if (!string.IsNullOrEmpty(flightSearchParameters.Airline))
-            {
-                query = query.Where(flight => flight.Airline == flightSearchParameters.Airline);
-            }
-            if (!string.IsNullOrEmpty(flightSearchParameters.DepartureCountry))
-            {
-                query = query.Where(flight => flight.DepartureCountry == flightSearchParameters.DepartureCountry);
-            }
-            if (!string.IsNullOrEmpty(flightSearchParameters.DestinationCountry))
-            {
-                query = query.Where(flight => flight.DestinationCountry == flightSearchParameters.DestinationCountry);
-            }
-            if (!string.IsNullOrEmpty(flightSearchParameters.DepartureAirport))
-            {
-                query = query.Where(flight => flight.DepartureAirport == flightSearchParameters.DepartureAirport);
-            }
-            if (!string.IsNullOrEmpty(flightSearchParameters.ArrivalAirport))
-            {
-                query = query.Where(flight => flight.ArrivalAirport == flightSearchParameters.ArrivalAirport);
-            }
-            if (flightSearchParameters.DepartureDate.HasValue)
-            {
-                query = query.Where(flight => flight.DepartureDate.Date == flightSearchParameters.DepartureDate.Value.Date);
-            }
-            if (flightSearchParameters.ArrivalDate.HasValue)
-            {
-                query = query.Where(flight => flight.ArrivalDate.Date == flightSearchParameters.ArrivalDate.Value.Date);
-            }
-            if (flightSearchParameters.AvailableSeats.HasValue)
-            {
-                query = query.Where(flight => flight.AvailableSeats >= flightSearchParameters.AvailableSeats.Value);
-            }
-            if (flightSearchParameters.Price.HasValue)
-            {
-                query = query.Where(flight => flight.ClassPrices.ContainsValue(flightSearchParameters.Price.Value) );
-            }
+            var query = DataStore.Flights
+                .Where(flight =>
+                    (string.IsNullOrEmpty(flightSearchParameters.Airline) ? true : flight.Airline == flightSearchParameters.Airline) &&
+                    (string.IsNullOrEmpty(flightSearchParameters.DepartureCountry) ? true : flight.DepartureCountry == flightSearchParameters.DepartureCountry) &&
+                    (string.IsNullOrEmpty(flightSearchParameters.DestinationCountry) ? true : flight.DestinationCountry == flightSearchParameters.DestinationCountry) &&
+                    (string.IsNullOrEmpty(flightSearchParameters.DepartureAirport) ? true : flight.DepartureAirport == flightSearchParameters.DepartureAirport) &&
+                    (string.IsNullOrEmpty(flightSearchParameters.ArrivalAirport) ? true : flight.ArrivalAirport == flightSearchParameters.ArrivalAirport) &&
+                    (!flightSearchParameters.DepartureDate.HasValue ? true : flight.DepartureDate.Date == flightSearchParameters.DepartureDate.Value.Date) &&
+                    (!flightSearchParameters.ArrivalDate.HasValue ? true : flight.ArrivalDate.Date == flightSearchParameters.ArrivalDate.Value.Date) &&
+                    (!flightSearchParameters.AvailableSeats.HasValue ? true : flight.AvailableSeats >= flightSearchParameters.AvailableSeats.Value) &&
+                    (!flightSearchParameters.Price.HasValue ? true : flight.ClassPrices.Values.Any(price => price == flightSearchParameters.Price.Value))
+                );
 
             return query.ToList();
         }
+
     }
 
 }
