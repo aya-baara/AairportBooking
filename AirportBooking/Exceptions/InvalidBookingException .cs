16dae6405a0e8a958898857fd229b8425a09e093
ayabaara <ayabaara4@gmail.com>
2025-03-13 10:38:48 +0200

    Refactor: Use default parameter for InvalidBookingException message to simplify constructors

--- AirportBooking/Exceptions/InvalidBookingException .cs
+++ AirportBooking/Exceptions/InvalidBookingException .cs
@@ -4,14 +4,8 @@ namespace AirportBooking.Exceptions
 {
     public class InvalidBookingException : Exception
     {
-        public InvalidBookingException() : base("Invalid booking operation.")
+        public InvalidBookingException(string message = "Invalid booking operation.") : base(message)
         {
         }
-
-        public InvalidBookingException(string message) : base(message)
-        {
-        }
-
     }
-
 }
